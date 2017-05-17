dba93d7d401b2a7386b8e8a305543dbc4678bed6
Roger's Mac <ipadaxe@gmail.com>
2017-05-17 22:21:34 +0800

    Initialize

---
+++ ChubbyBuster/ExerciseDataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ExerciseDataViewController.swift
+//  ChubbyBuster
+//
+//  Created by Roger's Mac on 2017/5/17.
+//  Copyright © 2017年 Roger's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
